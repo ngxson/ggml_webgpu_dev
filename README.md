1e7ab8488f23ccc3e3faaf108de2e6005d9552a6
ngxson <thichthat@gmail.com>
2024-06-25 21:08:00 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# ggml_webgpu_dev
+
+Testbed for ggml webgpu implementation (via emscripten)
+
+To build: `docker compose up`
+
+To run: `npx http-server build-web -p 9999 -c-1`
+
+Then, access http://127.0.0.1:9999
---
+++ copy.sh
@@ -0,0 +1,12 @@
+cp llama.cpp/ggml.c .
+cp llama.cpp/ggml.h .
+cp llama.cpp/ggml-alloc.c .
+cp llama.cpp/ggml-alloc.h .
+cp llama.cpp/ggml-backend.c .
+cp llama.cpp/ggml-backend.h .
+cp llama.cpp/ggml-quants.c .
+cp llama.cpp/ggml-quants.h .
+
+cp llama.cpp/ggml-backend-impl.h .
+cp llama.cpp/ggml-common.h .
+cp llama.cpp/ggml-impl.h .
