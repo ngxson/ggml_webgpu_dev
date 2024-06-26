3a4729591f3a77bae21f16b7d6494dc527db5a3f
ngxson <thichthat@gmail.com>
2024-06-27 00:09:24 +0200

    sync upstream

--- copy.sh
+++ copy.sh
@@ -1,12 +1,13 @@
-cp llama.cpp/ggml.c .
-cp llama.cpp/ggml.h .
-cp llama.cpp/ggml-alloc.c .
-cp llama.cpp/ggml-alloc.h .
-cp llama.cpp/ggml-backend.c .
-cp llama.cpp/ggml-backend.h .
-cp llama.cpp/ggml-quants.c .
-cp llama.cpp/ggml-quants.h .
+cp llama.cpp/ggml/src/ggml.c .
+cp llama.cpp/ggml/src/ggml-alloc.c .
+cp llama.cpp/ggml/src/ggml-backend.c .
+cp llama.cpp/ggml/src/ggml-quants.c .
+cp llama.cpp/ggml/src/ggml-quants.h .
 
-cp llama.cpp/ggml-backend-impl.h .
-cp llama.cpp/ggml-common.h .
-cp llama.cpp/ggml-impl.h .
+cp llama.cpp/ggml/include/ggml.h .
+cp llama.cpp/ggml/include/ggml-alloc.h .
+cp llama.cpp/ggml/include/ggml-backend.h .
+
+cp llama.cpp/ggml/src/ggml-backend-impl.h .
+cp llama.cpp/ggml/src/ggml-common.h .
+cp llama.cpp/ggml/src/ggml-impl.h .
