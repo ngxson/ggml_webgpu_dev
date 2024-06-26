8242c73aa8211453b6124d4d933779c014156365
ngxson <thichthat@gmail.com>
2024-06-26 12:12:26 +0200

    move to a more universal cpp impl

--- demo.cpp
+++ demo.cpp
@@ -1,10 +1,12 @@
 #include <GLFW/glfw3.h>
-#include <webgpu/webgpu_cpp.h>
 #include <iostream>
 #include <vector>
 #include <emscripten/emscripten.h>
 
-#define MIN_STORAGE_BUFFER_ALIGNMENT 256
+#define WEBGPU_CPP_IMPLEMENTATION
+#include "ggml-wgpu/webgpu.hpp"
+
+#define BUF_ALIGN 256
 #define N_TENSOR_PARAMS 4
 
 const int rows_A = 4, cols_A = 2;
@@ -84,79 +86,88 @@ var<uniform> tensor_params: TensorParams;
 @workgroup_size(1)
 fn kernel_add(@builtin(global_invocation_id) global_id: vec3<u32>) {
     let x = src0[global_id.x + tensor_params.offs_src0/4u];
-    let y = src0[global_id.x + tensor_params.offs_src1/4u];
+    let y = src1[global_id.x + tensor_params.offs_src1/4u];
             dest[global_id.x + tensor_params.offs_dest/4u] = x + y;
 }
 )";
 
 void Start() {
-    ctx->device.GetLimits(&ctx->limits);
-    ctx->queue = ctx->device.GetQueue();
+    ctx->device.getLimits(&ctx->limits);
+    ctx->queue = ctx->device.getQueue();
 
     // init bind group layout
-    wgpu::BindGroupLayoutEntry bgLayoutEntries[4] = {0};
+    wgpu::BindGroupLayoutEntry bgLayoutEntries[4];
     {
+        bgLayoutEntries[0].setDefault();
         bgLayoutEntries[0].binding = 0;
         bgLayoutEntries[0].visibility = wgpu::ShaderStage::Compute;
         bgLayoutEntries[0].buffer.type = wgpu::BufferBindingType::Storage;
         bgLayoutEntries[0].buffer.hasDynamicOffset = false;
         bgLayoutEntries[0].buffer.minBindingSize = 0;
 
+        bgLayoutEntries[1].setDefault();
         bgLayoutEntries[1].binding = 1;
         bgLayoutEntries[1].visibility = wgpu::ShaderStage::Compute;
         bgLayoutEntries[1].buffer.type = wgpu::BufferBindingType::Storage;
         bgLayoutEntries[1].buffer.hasDynamicOffset = false;
         bgLayoutEntries[1].buffer.minBindingSize = 0;
 
+        bgLayoutEntries[2].setDefault();
         bgLayoutEntries[2].binding = 2;
         bgLayoutEntries[2].visibility = wgpu::ShaderStage::Compute;
         bgLayoutEntries[2].buffer.type = wgpu::BufferBindingType::Storage;
         bgLayoutEntries[2].buffer.hasDynamicOffset = false;
         bgLayoutEntries[2].buffer.minBindingSize = 0;
 
+        bgLayoutEntries[3].setDefault();
         bgLayoutEntries[3].binding = 3;
         bgLayoutEntries[3].visibility = wgpu::ShaderStage::Compute;
         bgLayoutEntries[3].buffer.type = wgpu::BufferBindingType::Uniform;
         bgLayoutEntries[3].buffer.hasDynamicOffset = false;
         bgLayoutEntries[3].buffer.minBindingSize = N_TENSOR_PARAMS*sizeof(int32_t);
     }
-    wgpu::BindGroupLayoutDescriptor bindGroupLayoutDescriptor{
-        .label = "ggml-wgpu-bind-group-layout",
-        .entryCount = 4,
-        .entries = bgLayoutEntries,
+    wgpu::BindGroupLayoutDescriptor bglDesc = wgpu::Default;
+    {
+        bglDesc.label = "ggml-wgpu-bind-group-layout";
+        bglDesc.entryCount = 4;
+        bglDesc.entries = bgLayoutEntries;
     };
-    ctx->bind_group_layout = ctx->device.CreateBindGroupLayout(&bindGroupLayoutDescriptor);
+    ctx->bind_group_layout = ctx->device.createBindGroupLayout(bglDesc);
     GGML_ASSERT(ctx->bind_group_layout);
 
     // load shader
     {
-        wgpu::ShaderModuleWGSLDescriptor wgslDesc;
+        wgpu::ShaderModuleWGSLDescriptor wgslDesc = wgpu::Default;
         wgslDesc.code = SHADER_CODE;
         wgpu::ShaderModuleDescriptor shaderModuleDescriptor;
-        shaderModuleDescriptor.nextInChain = &wgslDesc;
-        ctx->shader_module = ctx->device.CreateShaderModule(&shaderModuleDescriptor);
+        shaderModuleDescriptor.nextInChain = (const WGPUChainedStruct *) &wgslDesc;
+        ctx->shader_module = ctx->device.createShaderModule(shaderModuleDescriptor);
         GGML_ASSERT(ctx->shader_module);
     }
 
     // create pipeline from shader
     {
-        wgpu::PipelineLayoutDescriptor desc0{
-            .label = "ggml-wgpu-pipeline-layout",
-            .bindGroupLayoutCount = 1,
-            .bindGroupLayouts = &ctx->bind_group_layout,
+        wgpu::PipelineLayoutDescriptor plDesc = wgpu::Default;
+        {
+            plDesc.label = "ggml-wgpu-pipeline-layout";
+            plDesc.bindGroupLayoutCount = 1;
+            plDesc.bindGroupLayouts = (const WGPUBindGroupLayout *) &ctx->bind_group_layout;
         };
-        ctx->pipeline_layout = ctx->device.CreatePipelineLayout(&desc0);
+        ctx->pipeline_layout = ctx->device.createPipelineLayout(plDesc);
         GGML_ASSERT(ctx->pipeline_layout);
 
-        wgpu::ComputePipelineDescriptor desc1{
-            .label = "pipeline_add",
-            .layout = ctx->pipeline_layout,
-            .compute = wgpu::ProgrammableStageDescriptor{
-                .module = ctx->shader_module,
-                .entryPoint = "kernel_add",
-            },
+        wgpu::ComputePipelineDescriptor cpDesc = wgpu::Default;
+        {
+            cpDesc.label = "pipeline_add";
+            cpDesc.layout = ctx->pipeline_layout;
+            wgpu::ProgrammableStageDescriptor psDesc = wgpu::Default;
+            {
+                psDesc.module = ctx->shader_module;
+                psDesc.entryPoint = "kernel_add";
+            }
+            cpDesc.compute = psDesc;
         };
-        ctx->pipeline_add = ctx->device.CreateComputePipeline(&desc1);
+        ctx->pipeline_add = ctx->device.createComputePipeline(cpDesc);
         GGML_ASSERT(ctx->pipeline_add);
     }
 
@@ -166,24 +177,26 @@ void Alloc() {
     printf("%s\n", __func__);
     // alloc buffers
     {
-        wgpu::BufferDescriptor desc0{
-            .label = "storage_buffer",
-            .usage = wgpu::BufferUsage::Storage
+        wgpu::BufferDescriptor desc0 = wgpu::Default;
+        {
+            desc0.label = "storage_buffer";
+            desc0.usage = wgpu::BufferUsage::Storage
                 | wgpu::BufferUsage::CopyDst
-                | wgpu::BufferUsage::CopySrc,
-            .size = MIN_STORAGE_BUFFER_ALIGNMENT*16u,
-            .mappedAtCreation = false, 
+                | wgpu::BufferUsage::CopySrc;
+            desc0.size = BUF_ALIGN*16u;
+            desc0.mappedAtCreation = false;
         };
-        ctx->buffer = ctx->device.CreateBuffer(&desc0);
+        ctx->buffer = ctx->device.createBuffer(desc0);
         GGML_ASSERT(ctx->buffer);
 
-        wgpu::BufferDescriptor desc1{
-            .label = "params_buffer",
-            .usage = wgpu::BufferUsage::Uniform | wgpu::BufferUsage::CopyDst,
-            .size = MIN_STORAGE_BUFFER_ALIGNMENT,
-            .mappedAtCreation = false, 
+        wgpu::BufferDescriptor desc1 = wgpu::Default;
+        {
+            desc1.label = "params_buffer";
+            desc1.usage = wgpu::BufferUsage::Uniform | wgpu::BufferUsage::CopyDst;
+            desc1.size = BUF_ALIGN;
+            desc1.mappedAtCreation = false;
         };
-        ctx->buf_tensor_params = ctx->device.CreateBuffer(&desc1);
+        ctx->buf_tensor_params = ctx->device.createBuffer(desc1);
         GGML_ASSERT(ctx->buffer);
     }
 
@@ -193,17 +206,17 @@ void Compute() {
     printf("%s\n", __func__);
     // set matrix data and params
     size_t offs_A = 0;
-    ctx->queue.WriteBuffer(ctx->buffer, offs_A, demo_mat_A, demo_mat_nbytes);
-    size_t offs_B = 1u*MIN_STORAGE_BUFFER_ALIGNMENT;
-    ctx->queue.WriteBuffer(ctx->buffer, offs_B, demo_mat_B, demo_mat_nbytes);
-    size_t offs_dest = 2u*MIN_STORAGE_BUFFER_ALIGNMENT;
-    //size_t offs_params = 3u*MIN_STORAGE_BUFFER_ALIGNMENT;
+    ctx->queue.writeBuffer(ctx->buffer, offs_A, demo_mat_A, demo_mat_nbytes);
+    size_t offs_B = 1u*BUF_ALIGN;
+    ctx->queue.writeBuffer(ctx->buffer, offs_B, demo_mat_B, demo_mat_nbytes);
+    size_t offs_dest = 2u*BUF_ALIGN;
+    //size_t offs_params = 3u*BUF_ALIGN;
     size_t offs_params = 0; // buf_tensor_params
     ctx->tensor_params_host[0] = 0;
     ctx->tensor_params_host[1] = 0;
     ctx->tensor_params_host[2] = 0;
     ctx->tensor_params_host[3] = 0; // unused for now
-    ctx->queue.WriteBuffer(ctx->buf_tensor_params, offs_params, ctx->tensor_params_host, N_TENSOR_PARAMS*sizeof(int32_t));
+    ctx->queue.writeBuffer(ctx->buf_tensor_params, offs_params, ctx->tensor_params_host, N_TENSOR_PARAMS*sizeof(int32_t));
 
     // set bind group entry
     auto & bgEntries = ctx->bgEntries;
@@ -211,98 +224,96 @@ void Compute() {
         bgEntries[0].binding = 0;
         bgEntries[0].buffer = ctx->buffer;
         bgEntries[0].offset = 0;
-        bgEntries[0].size = MIN_STORAGE_BUFFER_ALIGNMENT;
+        bgEntries[0].size = BUF_ALIGN;
 
         bgEntries[1].binding = 1;
         bgEntries[1].buffer = ctx->buffer;
         bgEntries[1].offset = offs_B;
-        bgEntries[1].size = MIN_STORAGE_BUFFER_ALIGNMENT;
+        bgEntries[1].size = BUF_ALIGN;
 
         bgEntries[2].binding = 2;
         bgEntries[2].buffer = ctx->buffer;
         bgEntries[2].offset = offs_dest;
-        bgEntries[2].size = MIN_STORAGE_BUFFER_ALIGNMENT;
+        bgEntries[2].size = BUF_ALIGN;
 
         bgEntries[3].binding = 3;
         bgEntries[3].buffer = ctx->buf_tensor_params;
         bgEntries[3].offset = offs_params;
-        bgEntries[3].size = MIN_STORAGE_BUFFER_ALIGNMENT;
+        bgEntries[3].size = BUF_ALIGN;
     }
-    wgpu::BindGroupDescriptor bindGroupDescriptor{
-        .label = "bind_group",
-        .layout = ctx->bind_group_layout,
-        .entryCount = 4,
-        .entries = ctx->bgEntries,
+    wgpu::BindGroupDescriptor bgDesc = wgpu::Default;
+    {
+        bgDesc.label = "bind_group";
+        bgDesc.layout = ctx->bind_group_layout;
+        bgDesc.entryCount = 4;
+        bgDesc.entries = ctx->bgEntries;
     };
-    auto bindGroup = ctx->device.CreateBindGroup(&bindGroupDescriptor);
+    auto bindGroup = ctx->device.createBindGroup(bgDesc);
     GGML_ASSERT(bindGroup);
 
     // compute
-    wgpu::CommandEncoderDescriptor desc0{
-        .label = "ggml_command_encoder",
-    };
-    auto commandEncoder = ctx->device.CreateCommandEncoder(&desc0);
+    wgpu::CommandEncoderDescriptor desc0 = wgpu::Default;
+    desc0.label = "ggml_command_encoder";
+    auto commandEncoder = ctx->device.createCommandEncoder(desc0);
     GGML_ASSERT(commandEncoder);
-    auto computePassEncoder = commandEncoder.BeginComputePass();
+    auto computePassEncoder = commandEncoder.beginComputePass();
     GGML_ASSERT(computePassEncoder);
-    computePassEncoder.SetPipeline(ctx->pipeline_add);
-    computePassEncoder.SetBindGroup(0, bindGroup, 0, NULL);
-    computePassEncoder.DispatchWorkgroups(demo_mat_nelements, 1, 1);
-    computePassEncoder.End();
+    computePassEncoder.setPipeline(ctx->pipeline_add);
+    computePassEncoder.setBindGroup(0, bindGroup, 0, NULL);
+    computePassEncoder.dispatchWorkgroups(demo_mat_nelements, 1, 1);
+    computePassEncoder.end();
 
-    auto cmdBuffer = commandEncoder.Finish();
+    auto cmdBuffer = commandEncoder.finish();
     GGML_ASSERT(cmdBuffer);
-    ctx->queue.Submit(1, &cmdBuffer);
-}
-
-static void handle_buffer_map(WGPUBufferMapAsyncStatus status, void * ready) {
-    printf("buffer_map status=%#.8x\n", status);
-    if (status == WGPUBufferMapAsyncStatus_Success) {
-        bool * _ready = (bool *) ready;
-        *_ready = true;
-    }
+    ctx->queue.submit(1, &cmdBuffer);
 }
 
 void GetResult(void * output) {
     printf("%s\n", __func__);
     // create tmp buffer
-    wgpu::BufferDescriptor desc0{
-        .label = "map_read_buffer",
-        .usage = wgpu::BufferUsage::MapRead | wgpu::BufferUsage::CopyDst,
-        .size = MIN_STORAGE_BUFFER_ALIGNMENT,
-        .mappedAtCreation = false, 
+    wgpu::BufferDescriptor desc0 = wgpu::Default;
+    {
+        desc0.label = "map_read_buffer";
+        desc0.usage = wgpu::BufferUsage::MapRead | wgpu::BufferUsage::CopyDst;
+        desc0.size = BUF_ALIGN;
+        desc0.mappedAtCreation = false;
     };
-    auto tmpbuf = ctx->device.CreateBuffer(&desc0);
+    auto tmpbuf = ctx->device.createBuffer(desc0);
 
     // command encoder
-    wgpu::CommandEncoderDescriptor desc1{
-        .label = "ggml_command_encoder_get_tensor",
-    };
-    auto commandEncoder = ctx->device.CreateCommandEncoder(&desc1);
-    size_t offs_dest = 2u*MIN_STORAGE_BUFFER_ALIGNMENT;
-    commandEncoder.CopyBufferToBuffer(ctx->buffer, offs_dest, tmpbuf, 0, demo_mat_nbytes);
+    wgpu::CommandEncoderDescriptor desc1 = wgpu::Default;
+    desc1.label = "ggml_command_encoder_get_tensor";
+    auto commandEncoder = ctx->device.createCommandEncoder(desc1);
+    size_t offs_dest = 2u*BUF_ALIGN;
+    commandEncoder.copyBufferToBuffer(ctx->buffer, offs_dest, tmpbuf, 0, demo_mat_nbytes);
 
     // run cmd
-    auto cmdBuffer = commandEncoder.Finish();
-    ctx->queue.Submit(1, &cmdBuffer);
+    auto cmdBuffer = commandEncoder.finish();
+    ctx->queue.submit(1, &cmdBuffer);
     bool ready = false;
-    tmpbuf.MapAsync(wgpu::MapMode::Read, 0, demo_mat_nbytes, handle_buffer_map, &ready);
+    auto ret = tmpbuf.mapAsync(wgpu::MapMode::Read, 0, demo_mat_nbytes, [&ready](WGPUBufferMapAsyncStatus status) {
+        printf("buffer_map status=%#.8x\n", status);
+        if (status == WGPUBufferMapAsyncStatus_Success) {
+            ready = true;
+        }
+    });
     while (!ready) {
         // https://eliemichel.github.io/LearnWebGPU/basic-3d-rendering/input-geometry/playing-with-buffers.html
         emscripten_sleep(1);
     }
 
     // get output buf
-    const void * buf = tmpbuf.GetConstMappedRange(0, demo_mat_nbytes);
+    const void * buf = tmpbuf.getConstMappedRange(0, demo_mat_nbytes);
     GGML_ASSERT(buf);
     memcpy(output, buf, demo_mat_nbytes);
 
     // free
-    tmpbuf.Unmap();
+    tmpbuf.unmap();
 }
 
+/*
 void GetAdapter(ggml_wgpu_context * ctx, void (*callback)(wgpu::Adapter)) {
-    ctx->instance.RequestAdapter(
+    ctx->instance.requestAdapter(
         nullptr,
         // TODO(https://bugs.chromium.org/p/dawn/issues/detail?id=1892): Use
         // wgpu::RequestAdapterStatus and wgpu::Adapter.
@@ -314,9 +325,12 @@ void GetAdapter(ggml_wgpu_context * ctx, void (*callback)(wgpu::Adapter)) {
             if (status != WGPURequestAdapterStatus_Success) {
                 exit(0);
             }
-            wgpu::Adapter adapter = wgpu::Adapter::Acquire(cAdapter);
+            wgpu::Adapter adapter = wgpu::Adapter::acquire(cAdapter);
             reinterpret_cast<void (*)(wgpu::Adapter)>(userdata)(adapter);
     }, reinterpret_cast<void*>(callback));
+
+    wgpu::RequestAdapterOptions opts;
+    ctx->instance.requestAdapter(opts);
 }
 
 void GetDevice(ggml_wgpu_context * ctx, void (*callback)(wgpu::Device)) {
@@ -329,7 +343,7 @@ void GetDevice(ggml_wgpu_context * ctx, void (*callback)(wgpu::Device)) {
             if (message) {
                 printf("RequestDevice: %s\n", message);
             }
-            wgpu::Device device = wgpu::Device::Acquire(cDevice);
+            wgpu::Device device = wgpu::Device::acquire(cDevice);
             device.SetUncapturedErrorCallback(
                 [](WGPUErrorType type, const char* message, void* userdata) {
                     std::cout << "Error: " << type << " - message: " << message;
@@ -338,30 +352,32 @@ void GetDevice(ggml_wgpu_context * ctx, void (*callback)(wgpu::Device)) {
             reinterpret_cast<void (*)(wgpu::Device)>(userdata)(device);
     }, reinterpret_cast<void*>(callback));
 }
+*/
 
 int main() {
     printf("Start\n");
     ctx = new ggml_wgpu_context;
-    ctx->instance = wgpu::CreateInstance();
+    ctx->instance = wgpuCreateInstance(nullptr); // descriptor not implemented yet in emscripten
+    //ctx->instance = wgpu::createInstance(&instanceDesc);
 
-    GetAdapter(ctx, [](wgpu::Adapter a) {
-        ctx->adapter = a;
-        GetDevice(ctx, [](wgpu::Device d) {
-            ctx->device = d;
-            Start();
-            Alloc();
-            Compute();
+    wgpu::RequestAdapterOptions opts = wgpu::Default;
+    auto adapter = ctx->instance.requestAdapter(opts);
 
-            std::vector<float> result(demo_mat_nelements);
-            GetResult(result.data());
+    wgpu::DeviceDescriptor deviceDesc = wgpu::Default;
+    ctx->device = adapter.requestDevice(deviceDesc);
 
-            printf("Result:\n");
-            for (int ir = 0; ir < rows_A; ir++) {
-                for (int ic = 0; ic < cols_A; ic++) {
-                    printf("%f, ", result[ir*cols_A + ic]);
-                }
-                printf("\n");
-            }
-        });
-    });
+    Start();
+    Alloc();
+    Compute();
+
+    std::vector<float> result(demo_mat_nelements);
+    GetResult(result.data());
+
+    printf("Result:\n");
+    for (int ir = 0; ir < rows_A; ir++) {
+        for (int ic = 0; ic < cols_A; ic++) {
+            printf("%f, ", result[ir*cols_A + ic]);
+        }
+        printf("\n");
+    }
 }
